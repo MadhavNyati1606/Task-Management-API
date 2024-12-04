e96c53fcbd4389d166cf6e7988ff95e75332b5b6
Madhav Nyati <madhavnyati1606@gmail.com>
2024-12-05 01:05:08 +0530

    Completed the CRUD functionality for the API

--- Task-Management-API/controllers/task.js
+++ Task-Management-API/controllers/task.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const Task = require("../models/task");
+const { NotFoundError, BadRequestError } = require("../errors");
 const getAllTasks = async (req, res) => {
   const tasks = await Task.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ tasks, number: tasks.length });
@@ -12,16 +13,58 @@ const createTask = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ task });
 };
 
-const getTask = async () => {
-  res.send("login User");
+const getTask = async (req, res) => {
+  const {
+    params: { id },
+    user: { userId },
+  } = req;
+  const task = await Task.findOne({ _id: id, createdBy: userId });
+
+  if (!task) {
+    throw new NotFoundError(`Task not found with id ${id}`);
+  }
+
+  res.status(StatusCodes.OK).json({ task });
 };
 
-const updateTask = async () => {
-  res.send("login User");
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const {
+    body: { title, category },
+    user: { userId },
+  } = req;
+
+  if (title === "" || category === "") {
+    throw new BadRequestError(
+      `Please provide a title and category for the task`
+    );
+  }
+  const task = await Task.findOneAndUpdate(
+    { _id: id, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!task) {
+    throw new NotFoundError(`Task not found with id ${id}`);
+  }
+
+  res.status(StatusCodes.OK).json({ task });
 };
 
-const deleteTask = async () => {
-  res.send("login User");
+const deleteTask = async (req, res) => {
+  const {
+    params: { id },
+    user: { userId },
+  } = req;
+
+  const task = await Task.findOneAndDelete({ _id: id, createdBy: userId });
+
+  if (!task) {
+    throw new NotFoundError(`Task not found with id ${id}`);
+  }
+
+  res.status(StatusCodes.OK).json({ task });
 };
 
 module.exports = {
