6dfaf3f9cc0d94f8d9038ca87bc5026ef879fb9c
Madhav Nyati <madhavnyati1606@gmail.com>
2024-12-06 14:16:42 +0530

    Added the logic for the query for numericFilters and also based on the completion of the task.

--- Task-Management-API/controllers/task.js
+++ Task-Management-API/controllers/task.js
@@ -2,18 +2,54 @@ const { StatusCodes } = require("http-status-codes");
 const Task = require("../models/task");
 const { NotFoundError, BadRequestError } = require("../errors");
 const getAllTasks = async (req, res) => {
-  const { category } = req.query;
+  const { category, sort, completed, numericFilters } = req.query;
   const queryObject = { createdBy: req.user.userId };
   if (category) {
     queryObject.category = category;
   }
+
+  if (completed) {
+    queryObject.completed = completed;
+  }
+
+  const operatorMap = {
+    ">": "$gt",
+    ">=": "$gte",
+    "=": "$eq",
+    "<=": "$lte",
+    "<": "$lt",
+    "!=": "$ne",
+  };
+
+  if (numericFilters) {
+    const filters = numericFilters
+      .split(",")
+      .map((item) => item.replace(/([a-zA-Z]+)([<>=!]+)(.+)/, "$1_$2_$3"));
+    for (let i = 0; i < filters.length; i++) {
+      const [field, operator, value] = filters[i].split("_");
+      queryObject[field] = { [operatorMap[operator]]: value }; //The one more bracket in the key area of the object is done so as to evaluate the inside expression. Without this, it will take the expression as the key which is not right
+      console.log(queryObject);
+    }
+  }
+
+  let result = Task.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
   const limit = req.query.limit ? parseInt(req.query.limit) : 10;
   const page = req.query.page ? parseInt(req.query.page) : 1;
   const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
-  const tasks = await Task.find(queryObject).limit(limit).skip(skip);
+  const tasks = await result;
 
   res.status(StatusCodes.OK).json({ tasks, number: tasks.length });
+  // res.status(StatusCodes.OK).json({ msg: "The response has been done" });
 };
 
 const createTask = async (req, res) => {
