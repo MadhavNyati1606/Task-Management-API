aac219b17d7f08f838308936900d7122b60f338f
Madhav Nyati <madhavnyati1606@gmail.com>
2024-12-08 17:11:23 +0530

    Added the error handling functionality for handling Cast errors and validation errors as well errors for duplicate values.

--- Task-Management-API/middleware/error-handler.js
+++ Task-Management-API/middleware/error-handler.js
@@ -1,10 +1,36 @@
+const { StatusCodes } = require("http-status-codes");
 const { CustomErrorAPI } = require("../errors");
 
 const errorHandler = (err, req, res, next) => {
-  if (err instanceof CustomErrorAPI) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  let customErrorResponse = {
+    msg: err.message || "Something went wrong. Please try again later",
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+  };
+
+  if (err.name === "ValidationError") {
+    customErrorResponse.msg = Object.values(err.errors)
+      .map((item) => {
+        return item.message;
+      })
+      .join(",");
+    customErrorResponse.statusCode = 400;
   }
-  return res.status(500).json({ msg: "Something went wrong" });
+
+  if (err.name === "CastError") {
+    (customErrorResponse.msg = `No task found with id ${err.value}`),
+      (customErrorResponse.statusCode = 404);
+  }
+
+  if (err.code === 11000) {
+    customErrorResponse.msg = `Duplicate values found of ${Object.keys(
+      err.keyValue
+    )}. Please try again with different values`;
+    customErrorResponse.statusCode = 400;
+  }
+
+  return res
+    .status(customErrorResponse.statusCode)
+    .json({ msg: customErrorResponse.msg });
 };
 
 module.exports = errorHandler;
